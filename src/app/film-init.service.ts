6987bb3dbf0d901917bc281dad9c6bc1477a3d41
John Williams <darumswizel@gmail.com>
2019-11-20 17:10:51 -0600

    Grabbed stream and dimension information from uploaded files

--- src/app/film-init.service.ts
+++ src/app/film-init.service.ts
@@ -6,26 +6,24 @@ declare var ffprobe: any;
   providedIn: 'root'
 })
 
-
-
 export class FilmInitService {
 	process(paths): void{
 		for (var i = 0; i < paths.length; i++){
 			ffprobe(paths[i], function grab(output){
-				infoSplit(output);
+				streamProcess(output);
 			});
 		}
 	}
   constructor() { }
 }
 
-function infoSplit(info){
+function streamProcess(info){
 	console.log('Info---\n' + info);
 	let streams = {
 		"total": [],
-		"v": [], 
-		"a": [], 
-		"s": []
+		"video": [], 
+		"audio": [], 
+		"sub": []
 	};
 	let width = 0;
         let height = 0;
@@ -40,5 +38,48 @@ function infoSplit(info){
                         streams["total"].push(lines[i]);
 		}
         }
-	console.log(streams["total"]);
+	for (var i = 0; i < streams["total"].length; i++){
+                if (vReg.test(streams["total"][i])){
+			let tempReg = /Stream #\d+:\d+.*: Video: /;
+                        let sizeReg = /, [0-9]+x[0-9]+/;
+                        let pieces = streams["total"][i].split(tempReg);
+                        let sizePieces = streams["total"][i].match(sizeReg);
+                        sizePieces = sizePieces[0].split('x');
+                        if(streams["video"].length == 0){
+                                height = sizePieces[1];
+                                let widthCut = sizePieces[0].split(', ');
+                                width = widthCut[1];
+                        }
+			streams["video"].push(pieces[1]);
+			//streams["video"].push(streams["total"][i]);
+                } else if (aReg.test(streams["total"][i])){
+                        let tempReg = /Stream #\d+:\d+\(/;
+                        let pieces = streams["total"][i].split(tempReg);
+			let outStream = "";
+                        if(pieces.length < 2){
+                                pieces = pieces[0].split(/Audio:/);
+                                outStream = 'Undefinded:' + pieces[1];
+                        } else{
+                                pieces = pieces[1].split(/\): Audio:/);
+                                outStream = pieces[0] + ':' + pieces[1];
+			}
+			streams["audio"].push(outStream);
+                } else if (sReg.test(streams["total"][i])){
+                        let tempReg = /Stream #\d+:\d+\(/;
+                        let pieces = streams["total"][i].split(tempReg);
+			let outStream = "";
+                        if(pieces.length < 2){
+                                pieces = pieces[0].split(/Subtitle:/);
+                                outStream = 'Undefinded:' + pieces[1];
+                        } else{
+                                pieces = pieces[1].split(/\): Subtitle:/);
+                                outStream = pieces[0] + ':' + pieces[1];
+                        }
+			streams["sub"].push(outStream);
+                } else {;}
+	}
+	console.log(streams["video"]);
+	console.log(streams["audio"]);
+	console.log(streams["sub"]);
+	console.log('Width: ' + width + ' Height: ' + height);
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,16 +6,6 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-	filename = 'No File';
-	vStreams = [];
-	aStreams = ['No Audio'];
-	sStreams = ['No Subtitle'];
-	start = '00:00:00.00';
-	duration = '1:00';
-	extension = ['mp4'];
-	cropW = 0;
-	cropH = 0;
-	scale = 0;
 
 	constructor() { }
 
